d6140cea9f6862acf48b159d257abd6f99ed34a3
Emwende <emwende89@gmail.com>
2022-09-22 13:08:49 -0700

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """a program that prints the result of the addition of all arguments"""
     import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-            print(result)
+
+    arg_count = len(sys.argv) - 1
+    arg_sum = 0
+
+    if arg_count > 1:
+        for x in range(1, arg_count + 1):
+            arg_sum += int(sys.argv[x])
+            print(arg_sum)
+        elif arg_count == 1:
+            print(int(sys.argv[1]))
+        else:
+            print(0)
