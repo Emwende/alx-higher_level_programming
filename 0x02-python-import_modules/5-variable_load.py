7fb0d345e938c0b6ce14617a75e0f76484650e4f
Emwende <emwende89@gmail.com>
2022-09-22 12:01:29 -0700

    fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 """a program that imports the variable a from the
 file variable_load_5.py and prints its value."""
 
 import variable_load_5
 a = variable_load_5.a
-print(a)
+print(a):wq
+
