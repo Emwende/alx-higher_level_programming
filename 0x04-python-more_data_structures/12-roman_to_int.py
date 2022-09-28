25d77cadd187390f4601fddfd0b99cb997a7ace8
Emwende <emwende89@gmail.com>
2022-09-28 12:20:12 -0700

    roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    number = 0
-    if roman_string is None or isinstance(roman_string, str) is False:
-        return number
-    else:
-        splitted = list(roman_string)
-        sliced = [conv[i] for i in splitted]
-        length = len(sliced)
-        count = 0
-        while count < length:
-            if count != length-1:
-                 if sliced[count] < sliced[count+1]:
-                     number += abs(sliced[count] - sliced[count+1])
-                     count += 2
-                      else:
-                          number += sliced[count]
-                          count += 1
-                      else:
-                          number += sliced[count]
-                          count += 1
-                          return number
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100
+            integer = 0
+            for i in range(len(roman_string)):
+            if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            integer += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+            else:
+            integer += roman[roman_string[i]]
+            return (integer)
