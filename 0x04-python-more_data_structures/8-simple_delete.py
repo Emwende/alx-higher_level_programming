fe969c0023a2b731069b16e71fa3d1ed84544a2b
Emwende <emwende89@gmail.com>
2022-09-28 11:29:48 -0700

    delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary.keys():
-        a_dictionary.pop(key)
-        return a_dictionary
+    try:
+        del a_dictionary[key]
+        return (a_dictionary)
+    except KeyError:
+        return (a_dictionary)
